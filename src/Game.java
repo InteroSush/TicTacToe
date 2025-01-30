7de5cf566060db0f8fcb7b72875b94baf096a439
InteroSush <101630735+InteroSush@users.noreply.github.com>
2025-01-30 13:33:50 +0300

    read, makeMove

--- src/Game.java
+++ src/Game.java
@@ -1,3 +1,4 @@
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -28,6 +29,11 @@ public class Game {
 
             printTurn();
             printField();
+
+            makeMove();
+
+            changeTurn();
+
         }
         if (gameStatus == GameStatus.DRAW) {
 
@@ -70,6 +76,37 @@ public class Game {
                 System.out.print(" " + field[x][y]);
             }
             System.out.println();
+        }
+    }
+
+    private int charToInt(char c) {
+        if (c == '1') return 0;
+        if (c == '2') return 1;
+        if (c == '3') return 2;
+        throw new IllegalArgumentException();
+    }
+
+    private int[] read() {
+        String input;
+        int[] out = new int[2];
+        while (true) {
+            String line = scanner.nextLine().strip();
+            if (line.matches("[123][123]")) {
+                out[0] = charToInt(line.charAt(0));
+                out[1] = charToInt(line.charAt(1));
+                break;
+            }
+            System.out.println("Invalid input!");
+        }
+        return out;
+    }
+
+    private void makeMove() {
+        int[] cords = read();
+        if (turn) {
+            field[cords[1]][cords[0]] = Cell.X;
+        } else {
+            field[cords[1]][cords[0]] = Cell.O;
         }
     }
 
